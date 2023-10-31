f7c033d8a7b258c48ae8df933af5d6cf447ffdd0
Juan2k-1 <jadova16@gmail.com>
2023-10-31 10:46:37 +0100

    Añadido el proyecto en spring inicial

---
+++ Proyecto_GestionTareas/Proyecto_GestionTareas/src/test/java/com/CMEPPS/Proyecto_GestionTareas/ProyectoGestionTareasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CMEPPS.Proyecto_GestionTareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoGestionTareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
