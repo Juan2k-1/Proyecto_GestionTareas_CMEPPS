59cd7b1cf11205f3a1593b6a93300aac716639a7
Juan2k-1 <jadova16@gmail.com>
2023-12-05 11:02:32 +0100

    Usuario y UsuarioEntity añadidos al proyecto

--- gestion_tareas/gestion_tareas/src/main/java/CMEPPS/practicaScrum/models/Usuario.java
+++ gestion_tareas/gestion_tareas/src/main/java/CMEPPS/practicaScrum/models/Usuario.java
@@ -6,5 +6,93 @@ package CMEPPS.practicaScrum.models;
  */
 public class Usuario
 {
+    private Long id;
+    private String nombre;
+    private String apellidos;
+    private String nickName;
+    private String salt;
+    private String hashContraseña;
+    private String email;
 
+    public Usuario(Long id, String nombre, String apellido, String email, String nickName, String hash_contraseña, String salt)
+    {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidos = apellido;
+        this.email = email;
+        this.nickName = nickName;
+        this.hashContraseña = hash_contraseña;
+        this.salt = salt;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Usuario{" + "id=" + id + ", nombre=" + nombre + ", apellidos=" + apellidos + ", nickName=" + nickName + ", salt=" + salt + ", hashContrase\u00f1a=" + hashContraseña + ", email=" + email + '}';
+    }
+    
+    public Long getId()
+    {
+        return id;
+    }
+
+    public String getNombre()
+    {
+        return nombre;
+    }
+
+    public String getApellidos()
+    {
+        return apellidos;
+    }
+
+    public String getNickName()
+    {
+        return nickName;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public String getSalt()
+    {
+        return salt;
+    }
+
+    public void setSalt(String salt)
+    {
+        this.salt = salt;
+    }
+
+    public String getHashContraseña()
+    {
+        return hashContraseña;
+    }
+
+    public void setHashContraseña(String hashContraseña)
+    {
+        this.hashContraseña = hashContraseña;
+    }
+
+    public void setNombre(String nombre)
+    {
+        this.nombre = nombre;
+    }
+
+    public void setApellidos(String apellidos)
+    {
+        this.apellidos = apellidos;
+    }
+
+    public void setNickName(String nickName)
+    {
+        this.nickName = nickName;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }   
 }
--- gestion_tareas/gestion_tareas/src/main/java/CMEPPS/practicaScrum/models/driver_adapter/UsuarioEntity.java
+++ gestion_tareas/gestion_tareas/src/main/java/CMEPPS/practicaScrum/models/driver_adapter/UsuarioEntity.java
@@ -1,9 +1,54 @@
 package CMEPPS.practicaScrum.models.driver_adapter;
 
+import CMEPPS.practicaScrum.models.Usuario;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 /**
  *
  * @author Juan Alberto Dominguez Vazquez
  */
-public class UsuarioEntity {
+@Entity
+public class UsuarioEntity
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
+    @Column(name = "nombre", nullable = false)
+    private String nombre;
+
+    @Column(name = "apellidos", nullable = false)
+    private String apellidos;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "nickName", nullable = false, unique = true)
+    private String nickName;
+
+    @Column(name = "hash_contraseña", nullable = false)
+    private String hashContraseña;
+
+    @Column(name = "salt", nullable = false)
+    private String salt;
+
+    public UsuarioEntity(Long id, String nombre, String apellidos, String email, String nickName, String hashContraseña, String salt)
+    {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.email = email;
+        this.nickName = nickName;
+        this.hashContraseña = hashContraseña;
+        this.salt = salt;
+    }
+
+    public Usuario toUsuario()
+    {
+        return new Usuario(this.id, this.nombre, this.apellidos, this.email, this.nickName, this.hashContraseña, this.salt);
+    }
 }
